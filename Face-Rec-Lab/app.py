d1d5d823064fda6d58405e5c6bbdac7da715ee3c
Noam Cohen-Ofir <109899445+noamco23-meet@users.noreply.github.com>
2022-12-06 17:46:04 +0200

    face recognition lab done
    hashtag yippee

--- Face-Rec-Lab/app.py
+++ Face-Rec-Lab/app.py
@@ -11,7 +11,14 @@ app = Flask(  # Create a flask app
 )
 
 config = {
-
+  "apiKey": "AIzaSyAEfpcAlxO6wtXzUnXWkFEsIeMV5K3YFXo",
+  "authDomain": "face-recognition-project-faa88.firebaseapp.com",
+  "projectId": "face-recognition-project-faa88",
+  "storageBucket": "face-recognition-project-faa88.appspot.com",
+  "messagingSenderId": "374763925057",
+  "appId": "1:374763925057:web:869d01db2af309fb4720c0",
+  "measurementId": "G-1K8V52LQK6",
+  "databaseURL": "https://face-recognition-project-faa88-default-rtdb.firebaseio.com/"
 }
 
 firebase = pyrebase.initialize_app(config)
@@ -19,22 +26,13 @@ auth = firebase.auth()
 db = firebase.database()
 
 app.config['SECRET_KEY'] = "Your_secret_string"
-UPLOAD_FOLDER = 'static/images/faces'
+UPLOAD_FOLDER = 'images\\faces\\'
 ALLOWED_EXTENSIONS = ['png', 'jpg', 'jpeg']
-
-
-@app.route('/', methods=['GET', 'POST'])
-def login():
-	if request.method == "POST":
-
-		return redirect(url_for("home"))
-	else:
-  	return render_template('login.html')
 
 
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', person=db.child("Users").child(login_session['user']['localId']).get().val())
 
 
 @app.route('/signup', methods=['GET', 'POST'])
@@ -46,7 +44,7 @@ def signup():
         upload_file(face)
         try:
             login_session['user'] = auth.create_user_with_email_and_password(email, password)
-            person = {"email": email, "face": face.filename}
+            person = {"email": email, "face": face.filename, "password": password}
             db.child("Users").child(login_session['user']['localId']).set(person)
             return redirect(url_for('login'))
         except:
@@ -54,6 +52,34 @@ def signup():
     else:
         return render_template('signup.html')
 
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method=='POST':
+        email = request.form['email']
+        password = request.form['password']
+        face = request.files['face']
+        try:
+            login_session['user'] = auth.sign_in_with_email_and_password(email, password)
+        except:
+            error = "Your credentials didn't match"
+            return render_template("login.html", error=error)
+        print("intest")
+        saved_face_path = os.path.join(app.static_folder,UPLOAD_FOLDER, db.child("Users").child(login_session['user']['localId']).child("face").get().val())
+        print(saved_face_path)
+        saved_face = face_recognition.load_image_file(saved_face_path)
+        saved_face_enc = face_recognition.face_encodings(saved_face)[0]
+        print(saved_face_enc)
+        input_face = face_recognition.load_image_file(face)
+        input_face_enc = face_recognition.face_encodings(input_face)[0]
+        print(input_face_enc)
+        results = face_recognition.compare_faces([saved_face_enc], input_face_enc)
+        if results[0]==True:
+            return redirect(url_for('home'))
+        return render_template("login.html", error="The faces don't match")
+    else:
+        return render_template("login.html")
+
+        
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -64,7 +90,8 @@ def upload_file(file):
     if request.method == 'POST':
         if file and allowed_file(file.filename):
             filename = file.filename
-            file.save(UPLOAD_FOLDER + "/" + filename)
+            fn = os.path.join(app.static_folder,UPLOAD_FOLDER, filename)
+            file.save(fn)
 
 
 if __name__ == "__main__":  # Makes sure this is the main process
